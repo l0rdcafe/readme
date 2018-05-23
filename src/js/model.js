2124830e4d737ab9f04bccf474672fca16528b76
Ismail Arafa <ismailarafa18@gmail.com>
2018-05-23 21:14:37 +0200

    add state.playing.duration

--- src/js/model.js
+++ src/js/model.js
@@ -11,6 +11,13 @@ const resetState = function() {
 };
 
 const setUserInfo = function(res) {
+  function convertMillisToMinsSecs(millis) {
+    const mins = Math.floor(millis / 60000);
+    const secs = ((millis % 60000) / 1000).toFixed(0);
+    const duration = `${mins}:${secs < 10 ? "0" : ""}${secs}`;
+    return duration;
+  }
+
   const name = res[0].display_name;
   const { id } = res[0];
   const isPlaying = res[1].is_playing;
@@ -20,9 +27,12 @@ const setUserInfo = function(res) {
   if (isPlaying) {
     const song = res[1].item.name;
     const artist = res[1].item.artists[0].name;
+    const duration = convertMillisToMinsSecs(res[1].item.duration_ms);
+
     state.playing = {
       song,
       artist,
+      duration,
       isPlaying: true
     };
   } else {
